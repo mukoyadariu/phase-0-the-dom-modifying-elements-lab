15b25d432bc5e0e001303db5bad80f19ed983b57
mukoyadarius <mukoya.darius@student.moringaschool.com>
2023-07-20 20:19:42 +0300

    completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+// index.js
+
+// Remove the <main> element with id "main"
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id attribute of the new <h1> element to "victory"
+newHeader.id = "victory";
+
+// Set the text content of the new <h1> element
+// Replace "YOUR-NAME" with your actual name
+const yourName = "Your Name"; // Replace "Your Name" with your actual name
+newHeader.textContent = `${yourName} is the champion`;
+
+// Append the new <h1> element to the body
+document.body.appendChild(newHeader);
